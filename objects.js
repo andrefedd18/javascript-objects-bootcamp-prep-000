d4a3d3026f7708de7f3565b015bee1fb16a87b51
alfedd Fedd <alfedd@crimson.ua.edu>
2018-10-16 03:13:38 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,7 @@ function updatePlaylist (playlist, artist, song) {
   playlist[artist]= song 
   return playlist
   }
-  function removeFromPlaylist (playlist, artist)
+  function removeFromPlaylist (playlist, artist) {
   delete playlist[artist]
   return playlist
+  }
