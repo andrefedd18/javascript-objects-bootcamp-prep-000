31a44164d35919871b443c7a62c018259c05307d
alfedd Fedd <alfedd@crimson.ua.edu>
2018-10-16 02:46:45 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,4 @@
+var playlist = {artist: "Minnie Riperton", };
+
+object.assign( {} , playlist, { artist: [' Minnie Riperton '] })
+object.updatePlaylist( {} , playlist, {artist: ['Minnie Riperton'], songTitle: ['Feeling That Your Feelin Good'] })
